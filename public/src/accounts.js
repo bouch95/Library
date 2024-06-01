41c5f50dd43fa4db5715e897e2cc42273f1f2aaa
bouch95 <167940846+bouch95@users.noreply.github.com>
2024-06-01 12:06:38 -0400

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -50,44 +50,41 @@ function getBooksPossessedByAccount(account, books, authors) {
 
 //////// TEST  TEST  TEST  /////////
 
-function TESTgetBooksPossessedByAccountTEST(account, books, authors) {
-   
-  let results = books.filter((book) => 
-    book.borrows.find((borrow) => borrow.id === account.id && !borrow.returned));
-  //Check to see if found the right book
-  //console.log(results);
-  //return true;
-  
-  const listBooks = [];
-  for (let i = 0; i < results.length; i++) {
-    const foundAuthor = authors.find((author) => results[i].authorId === author.id);
-  //Verfy we matching authors
-  //console.log(author.id);
-  //return true;
-    const foundBorrows = results[i].borrows;
-
-    const foundBook = results[i];
+const bookTests = [
+  {
+    id: "5f447132d487bd81da01e25e",
+    title: "sit eiusmod occaecat eu magna",
+    genre: "Science",
+    authorId: 8,
+    borrows: [
+      {
+        id: "5f446f2e2cfa3e1d234679b9",
+        returned: false,
+      },
+]
     
-    const frontBook = {
-      id: foundBook.id,
-      title: foundBook.title,
-      genre: foundBook.genre,
-      authorId: foundBook.authorId,
-    };
-  //verity correct part and structure of book info
-  //console.log(frontBook);
-  //return true;  
-    const combinedObjects = {
-      ...frontBook,
-      author: foundAuthor,
-      borrows: foundBorrows,
-    };
-    listBooks.push(combinedObjects); 
-  }  
-   return listBooks;
-}
+const accountTests = [
+  {
+    id: "5f446f2e2cfa3e1d234679b9",
+    picture: "https://api.adorable.io/avatars/75/esther.tucker@zillacon.me",
+    age: 25,
+    name: {
+      first: "Esther",
+      last: "Tucker",
+    },
+    company: "ZILLACON",
+    email: "esther.tucker@zillacon.me",
+    registered: "Thursday, May 28, 2015 2:51 PM",
+  },
+]
 
-  //console.log(TESTgetBooksPossessedByAccountTEST(account, books, authors));
+function BooksByAccount(booksTests, accountsTests) {
+   
+  let results = bookTests.filter((bookTest) => 
+    bookTest.borrows.find((borrow) => borrow.id === accountTest.id && !borrow.returned));
+  //Check to see if found the right book
+  return true;
+console.log(BooksByAccount(booKsTests, accountsTests));
 
   module.exports = {
   findAccountById,
