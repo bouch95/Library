51d3768a32e5de7df7f5970a55d2d8557b1c8eee
bouch95 <167940846+bouch95@users.noreply.github.com>
2024-06-06 20:40:45 -0400

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -26,65 +26,17 @@ function getBooksPossessedByAccount(account, books, authors) {
   const listBooks = [];
   for (let i = 0; i < results.length; i++) {
     const foundAuthor = authors.find((author) => results[i].authorId === author.id);
-  
-    const foundBorrows = results[i].borrows;
-
-    const foundBook = results[i];
-    
-    const frontBook = {
-      id: foundBook.id,
-      title: foundBook.title,
-      genre: foundBook.genre,
-      authorId: foundBook.authorId,
-    };
-        
-    const combinedObjects = {
-      ...frontBook,
-      author: foundAuthor,
-      borrows: foundBorrows,
-    };
-    listBooks.push(combinedObjects); 
+    listBooks.push(combine(results[i], foundAuthor)); 
   }  
    return listBooks;
 }
 
-//////// TEST  TEST  TEST  /////////
-
-const bookTests = [
-  {
-    id: "5f447132d487bd81da01e25e",
-    title: "sit eiusmod occaecat eu magna",
-    genre: "Science",
-    authorId: 8,
-    borrows: [
-      {
-        id: "5f446f2e2cfa3e1d234679b9",
-        returned: false,
-      },
-]
-    
-const accountTests = [
-  {
-    id: "5f446f2e2cfa3e1d234679b9",
-    picture: "https://api.adorable.io/avatars/75/esther.tucker@zillacon.me",
-    age: 25,
-    name: {
-      first: "Esther",
-      last: "Tucker",
-    },
-    company: "ZILLACON",
-    email: "esther.tucker@zillacon.me",
-    registered: "Thursday, May 28, 2015 2:51 PM",
-  },
-]
-
-function BooksByAccount(booksTests, accountsTests) {
-   
-  let results = bookTests.filter((bookTest) => 
-    bookTest.borrows.find((borrow) => borrow.id === accountTest.id && !borrow.returned));
-  //Check to see if found the right book
-  return true;
-console.log(BooksByAccount(booKsTests, accountsTests));
+function combine(foundBook, foundAuthor) {
+  return ({
+           ...foundBook, 
+           author: foundAuthor
+         });
+}
 
   module.exports = {
   findAccountById,
